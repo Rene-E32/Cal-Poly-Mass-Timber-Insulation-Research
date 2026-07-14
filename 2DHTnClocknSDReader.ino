f8d57fc79688c19752b2e08b2dabccc318aa1ab2
Rene-E32 <reneespinoza7676@gmail.com>
2026-07-14 11:36:19 -0700

    Add files via upload

--- 2DHTnClocknSDReader.ino
+++ 2DHTnClocknSDReader.ino
@@ -8,16 +8,9 @@
 
 #include <SD.h>
 
-//testing ignore this
-
 
 //naming/activating file for sd reader
 File sensorLog;
-
-
-//initilize their smog sensor
-//const int smogPin = A0;
-
 
 const 
 int chipSelect = 10; //for RTC
@@ -71,7 +64,7 @@ if (sensorLog.size() ==
 
 
 //setting headers
-sensorLog.println(F("Date(M/D/Y),Time(H:M:S),Internal Temperature(F),Internal Humidity(%),External Temperature(F),External Humidity(%),Smog(PPM)"));
+sensorLog.println(F("Date(M/D/Y),Time(H:M:S),Inside Temperature(C),Inside Temperature(F),Inside Humidity(%),Outside Temperature(C),Outside Temperature(F),Outside Humidity(%)"));
 
 Serial.println(F("New file created. Headers written."));
 
@@ -113,50 +106,34 @@ F(__TIME__)));
 
 }
 
-
-
 void 
 loop() {
 
 // initialize variables, gives 0 valuse
 //updates over 0, to make sure dont get NaN values
 
-int internalTemperature = 
-0;
+int internalTemperatureC = 0;
+int internalTemperatureF = 0;
 
-int internalHumidity = 
-0;
+int internalHumidity = 0;
 
-int externalTemperature = 
-0;
+int externalTemperatureC = 0;
+int externalTemperatureF = 0;
 
-int externalHumidity = 
-0;
-
-//int smogPPM = 0;
-
+int externalHumidity = 0;
 
 
 DateTime now = rtc.now(); // sets now time, the current moment of RTC
-
-
-
-// gathering smog readings from sensor
-
-//smogPPM = analogRead(smogPin);
-
 
 
 // internal/external temp/hum sensor setup, 
 //requests data
 
 int internalResults = 
-dhtInternal.readTemperatureHumidity(internalTemperature, internalHumidity);
+dhtInternal.readTemperatureHumidity(internalTemperatureC, internalHumidity);
 
 int externalResults = 
-dhtExternal.readTemperatureHumidity(externalTemperature, externalHumidity);
-
-
+dhtExternal.readTemperatureHumidity(externalTemperatureC, externalHumidity);
 
 // internal sensor serial printing
 
@@ -165,12 +142,16 @@ if (internalResults ==
 
 // convert from celsius to farenheight
 
-internalTemperature = (internalTemperature * 
+internalTemperatureF = (internalTemperatureC * 
 1.8) + 32;
 
-Serial.print(F("Internal Temperature: "));
+Serial.print(F("Internal Temperature(C): "));
 
-Serial.print(internalTemperature);
+Serial.print(internalTemperatureC);
+
+Serial.print(F(", Internal Temperature(F): "));
+
+Serial.print(internalTemperatureC);
 
 Serial.print(F(", Internal Humidity: "));
 
@@ -195,12 +176,16 @@ if (externalResults ==
 
 // convert from celsius to farenheight
 
-externalTemperature = (externalTemperature * 
+externalTemperatureF = (externalTemperatureC * 
 1.8) + 32;
 
-Serial.print(F("External Temperature: "));
+Serial.print(F("External Temperature(C): "));
 
-Serial.print(externalTemperature);
+Serial.print(externalTemperatureC);
+
+Serial.print(F(", External Temperature(F): "));
+
+Serial.print(externalTemperatureF);
 
 Serial.print(F(", External Humidity: "));
 
@@ -210,7 +195,7 @@ Serial.println(F(" %"));
 
 }
 
-else {// error for out sensor
+else { // error for out sensor
 
 Serial.println(DHT11::getErrorString(externalResults));
 
@@ -219,7 +204,6 @@ Serial.println(DHT11::getErrorString(externalResults));
 
 
 // Open the file to add the current sensor readings
-
 sensorLog = SD.open("logged.txt", FILE_WRITE); // if exists
 
 if (sensorLog) {
@@ -248,7 +232,11 @@ sensorLog.print(now.second(), DEC);
 
 sensorLog.print(",");
 
-sensorLog.print(internalTemperature);
+sensorLog.print(internalTemperatureC);
+
+sensorLog.print(",");
+
+sensorLog.print(internalTemperatureF);
 
 sensorLog.print(",");
 
@@ -256,18 +244,18 @@ sensorLog.print(internalHumidity);
 
 sensorLog.print(",");
 
-sensorLog.print(externalTemperature);
+sensorLog.print(externalTemperatureC);
+
+sensorLog.print(",");
+
+sensorLog.print(externalTemperatureF);
 
 sensorLog.print(",");
 
 sensorLog.println(externalHumidity); //very last one has to have LN
 //gives nice coloums, makes it easier to import to mat lab
 //m
-
-//sensorLog.print(",");
-
-//sensorLog.println(smogPPM); 
-
+ 
 
 sensorLog.close(); // save and close file, 
 // avoids corrupt whole file
@@ -282,288 +270,6 @@ Serial.println(F("Error opening logged.txt during loop data save."));
 
 
 
-delay(60000); // #include <DHT11.h>
-
-#include "RTClib.h"
-
-#include <Wire.h>
-
-#include <SPI.h>
-
-#include <SD.h>
-
-
-//naming/activating file for sd reader
-File sensorLog;
-
-
-//initilize their smog sensor
-//const int smogPin = A0;
-
-
-const 
-int chipSelect = 10; //for RTC
-
-const 
-int internalHumtempPin = 8;       //pin select/initilizaion for sensors
-
-const 
-int externalHumtempPin = 9;
-
-
-
-RTC_DS3231 rtc;  //something about clock
-
-DHT11 dhtInternal(internalHumtempPin);
-                                          //naming functions                     
-DHT11 dhtExternal(externalHumtempPin);
-
-
-
-void 
-setup() {
-
-
-Serial.begin(9600);
-
-
-
-// Initialize SD Card hardware
-
-if (!SD.begin(chipSelect)) {
-
-Serial.println(F("SD initialization failed!"));
-
-return;
-
-}
-
-Serial.println(F("SD initialization done."));
-
-
-
-// Create file and write CSV headers if it's a new file
-
-sensorLog = SD.open("logged.txt", FILE_WRITE);
-
-if (sensorLog) {
-
-if (sensorLog.size() ==
-0) { //"if file exists"
-
-
-//setting headers
-sensorLog.println(F("Date(M/D/Y),Time(H:M:S),Internal Temperature(F),Internal Humidity(%),External Temperature(F),External Humidity(%),Smog(PPM)"));
-
-Serial.println(F("New file created. Headers written."));
-
-} else { // if exist, with same name, itll add onto instead of making new
-
-Serial.println(F("File already exists. Appending new data to the end."));
-
-}
-
-sensorLog.close(); // log then close
-
-} else { //error handling
-
-Serial.println(F("Error opening logged.txt for header initialization."));
-
-}
-
-
-if (!rtc.begin()) { //begins clock
-
-Serial.println(F("Couldn't find RTC")); // if doesn't start
-
-while (1); // halt if RTC isn't wired correctly, kicks out if cant find 
-
-}
-
-
-
-if (rtc.lostPower()) { //lost power case
-
-Serial.println(F("RTC lost power, setting the time!"));
-
-// sets RTC to the date & time this sketch was compiled
-
-rtc.adjust(DateTime(F(__DATE__), //logs data from pc from upload
-F(__TIME__)));
-
-}
-
-}
-
-
-
-void 
-loop() {
-
-// initialize variables, gives 0 valuse
-//updates over 0, to make sure dont get NaN values
-
-int internalTemperature = 
-0;
-
-int internalHumidity = 
-0;
-
-int externalTemperature = 
-0;
-
-int externalHumidity = 
-0;
-
-//int smogPPM = 0;
-
-
-
-DateTime now = rtc.now(); // sets now time, the current moment of RTC
-
-
-
-// gathering smog readings from sensor
-
-//smogPPM = analogRead(smogPin);
-
-
-
-// internal/external temp/hum sensor setup, 
-//requests data
-
-int internalResults = 
-dhtInternal.readTemperatureHumidity(internalTemperature, internalHumidity);
-
-int externalResults = 
-dhtExternal.readTemperatureHumidity(externalTemperature, externalHumidity);
-
-
-
-// internal sensor serial printing
-
-if (internalResults == 
-0 ) {
-
-// convert from celsius to farenheight
-
-internalTemperature = (internalTemperature * 
-1.8) + 32;
-
-Serial.print(F("Internal Temperature: "));
-
-Serial.print(internalTemperature);
-
-Serial.print(F(", Internal Humidity: "));
-
-Serial.print(internalHumidity);
-
-Serial.println(F(" %"));
-
-}
-
-else { //function for error handling 
-
-Serial.println(DHT11::getErrorString(internalResults));
-
-}
-
-
-
-// external sensor serial printing 
-
-if (externalResults == 
-0 ) {
-
-// convert from celsius to farenheight
-
-externalTemperature = (externalTemperature * 
-1.8) + 32;
-
-Serial.print(F("External Temperature: "));
-
-Serial.print(externalTemperature);
-
-Serial.print(F(", External Humidity: "));
-
-Serial.print(externalHumidity);
-
-Serial.println(F(" %"));
-
-}
-
-else {// error for out sensor
-
-Serial.println(DHT11::getErrorString(externalResults));
-
-}
-
-
-
-// Open the file to add the current sensor readings
-
-sensorLog = SD.open("logged.txt", FILE_WRITE); // if exists
-
-if (sensorLog) {
-//writes all this
-sensorLog.print(now.month(), DEC); //DEC for decimial formating
-                              
-sensorLog.print('/');
-
-sensorLog.print(now.day(), DEC);
-
-sensorLog.print('/');
-
-sensorLog.print(now.year(), DEC);
-
-sensorLog.print(F(","));
-
-sensorLog.print(now.hour(), DEC);
-
-sensorLog.print(':');
-
-sensorLog.print(now.minute(), DEC);
-
-sensorLog.print(':');
-
-sensorLog.print(now.second(), DEC);
-
-sensorLog.print(",");
-
-sensorLog.print(internalTemperature);
-
-sensorLog.print(",");
-
-sensorLog.print(internalHumidity);
-
-sensorLog.print(",");
-
-sensorLog.print(externalTemperature);
-
-sensorLog.print(",");
-
-sensorLog.println(externalHumidity); //very last one has to have LN
-//gives nice coloums, makes it easier to import to mat lab
-//m
-
-//sensorLog.print(",");
-
-//sensorLog.println(smogPPM); 
-
-
-sensorLog.close(); // save and close file, 
-// avoids corrupt whole file
-
-Serial.println(F("Data successfully logged to SD card.")); //
-
-} else {
-
-Serial.println(F("Error opening logged.txt during loop data save."));
-
-}
-
-
-
-delay(60000); // 60000 mili secs = 1 minute  
-}
+delay(5000); // 5 seconds
+ 
 }
